e1bf4ae838692b8ac4c06f170ce70170625662a4
zhouyy <413993459@qq.com>
2018-07-23 20:11:51 +0800

    shoppingmall

---
+++ src/components/pages/ShoppingMall.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div class="search-bar">
+      <van-row>
+        <van-col span="3">
+          <img :src="locationIcon" width="80%" class="search-icon">
+        </van-col>
+        <van-col span="16">
+          <input type="text" class="search-input">
+        </van-col>
+        <van-col span="5">
+          <van-button size="mini">查找</van-button>
+        </van-col>
+      </van-row>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        'msg': 'Shopping mall',
+        'locationIcon': require('../../assets/images/location.png')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.search-bar {
+  height: 2.2rem;
+  line-height: 2.2rem;
+  background-color: #e5017d;
+}
+.search-icon {
+  padding-top: .2rem;
+  padding-left: .3rem;
+}
+.search-input {
+  width: 100%;
+  height: 1.3rem;
+  border-top: 0;
+  border-left: 0;
+  border-right: 0;
+  border-bottom: 1px solid #fff !important;
+  background-color: #e5017d;
+  color: #fff;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import ShoppingMall from '@/components/pages/ShoppingMall'
+// 全局引入vant
+// import Vant from 'vant'
+// import 'vant/lib/vant-css/index.css'
+// Vue.use(Vant)
+
+// 按需引入vant，使用babel-plugin-import插件
+import {Button, Row, Col} from 'vant'
+Vue.use(Router).use(Button).use(Row).use(Col)
+
+export default new Router({
+  routes: [
+    {path: '/', name: 'ShoppingMall',component: ShoppingMall}
+  ]
+})
